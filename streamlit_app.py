eb04bac080ba57bd2c7b5c20aadcea3a73dc3d66
t.busch <t.busch@fz-juelich.de>
2022-03-17 14:38:19 +0100

    automated commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,23 @@ import pyomo
 import plotly
 
 st.write("Hello world!")
+
+model = pyo.ConcreteModel()
+
+model.x = pyo.Var([1,2], domain=pyo.NonNegativeReals)
+
+model.OBJ = pyo.Objective(expr = 2*model.x[1] + 3*model.x[2])
+
+model.Constraint1 = pyo.Constraint(expr = 3*model.x[1] + 4*model.x[2] >= 1)
+
+st.write(type(model))
+
+
+solver = "glpk"
+optimizer = opt.SolverFactory(solver)
+
+try:
+    solver_info = optimizer.solve(model, tee=True)
+    st.write(type(solver_info))
+except Exception as e:
+    st.write(e)
